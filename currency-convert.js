93515cfea3a102038e0c1ae96381a671492010a0
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-04-18 20:28:04 +0530

    Handling errors and awaiting async function

--- currency-convert.js
+++ currency-convert.js
@@ -1,33 +1,47 @@
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`https://api.fixer.io/latest?base=${from}`).then(res => {
-    return res.data.rates[to];
+const getExchangeRate = async (from, to) => {
+  try {
+    const res = await axios.get(`https://api.fixer.io/latest?base=${from}`);
+    const rate = res.data.rates[to];
+
+    if (rate) {
+      return rate;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+  }
+};
+
+const getCountries = async currencyCode => {
+  try {
+    const res = await axios.get(
+      `https://restcountries.eu/rest/v2/currency/${currencyCode}`
+    );
+    return res.data.map(country => country.name);
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`);
+  }
+};
+
+//Using promises
+const convertCurrency = (from, to, amount) => {
+  let countries;
+  return getCountries(to).then(tempCountries => {
+    countries = tempCountries;
+    return getExchangeRate(from, to).then(rate => {
+      const exchangedAmount = rate * amount;
+
+      return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(
+        ', '
+      )}`;
+    });
   });
 };
 
-const getCountries = currencyCode => {
-  return axios
-    .get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-    .then(res => {
-      return res.data.map(country => country.name);
-    });
-};
-
-// const convertCurrency = (from, to, amount) => {
-//   let countries;
-//   return getCountries(to).then(tempCountries => {
-//     countries = tempCountries;
-//     return getExchangeRate(from, to).then(rate => {
-//       const exchangedAmount = rate * amount;
-
-//       return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(
-//         ', '
-//       )}`;
-//     });
-//   });
-// };
-
+//Using async/await
 const convertCurrencyAlt = async (from, to, amount) => {
   const countries = await getCountries(to);
   const rate = await getExchangeRate(from, to);
@@ -41,4 +55,4 @@ const convertCurrencyAlt = async (from, to, amount) => {
 
 convertCurrencyAlt('USD', 'INR', 2)
   .then(status => console.log(status))
-  .catch(e => console.log(e));
+  .catch(e => console.log(e.message));
